5a1250f07a84fef7bcea86f39f8d31f68a44e1fb
Chris James <qui666@gmail.com>
2019-01-23 14:59:13 +0000

    use external test package for cookme

--- cookme_test.go
+++ cookme_test.go
@@ -1,26 +1,27 @@
-package cookme
+package cookme_test
 
 import (
 	"bytes"
 	"fmt"
+	"github.com/quii/monolith-to-micro"
 	"testing"
 	"time"
 )
 
 func TestListIngredients(t *testing.T) {
 
-	var someIngredients = []Ingredient{
-		{"Milk", time.Now().Add(72 * time.Hour)},
-		{"Cheese", time.Now().Add(48 * time.Hour)},
+	var someIngredients = []cookme.Ingredient{
+		{Name: "Milk", ExpirationDate: time.Now().Add(72 * time.Hour)},
+		{Name: "Cheese", ExpirationDate: time.Now().Add(48 * time.Hour)},
 	}
 
-	var StubIngredientsRepo = func() Ingredients {
+	var StubIngredientsRepo = func() cookme.Ingredients {
 		return someIngredients
 	}
 
 	t.Run("prints ingredients from ingredients list ordered by exp date", func(t *testing.T) {
 		var got bytes.Buffer
-		ListIngredients(&got, IngredientsRepoFunc(StubIngredientsRepo))
+		cookme.ListIngredients(&got, cookme.IngredientsRepoFunc(StubIngredientsRepo))
 
 		want := fmt.Sprintf("%s\n%s\n", someIngredients[0], someIngredients[1])
 
